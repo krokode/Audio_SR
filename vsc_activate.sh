72e5f0f7e3618545b9e6535d9543c15ab7d80dde
krokode <krokodeele@gmail.com>
2025-11-01 00:03:12 +0100

    gitignore

--- vsc_activate.sh
+++
@@ -1,2 +0,0 @@
-module load cluster/genius/gpu_p100 PyTorch/1.12.1-foss-2022a-CUDA-11.7.0 SciPy-bundle/2022.05-foss-2022a h5py/3.7.0-foss-2022a matplotlib/3.5.2-foss-2022a
-. .venv/bin/activate
--- vsc_setup.sh
+++
@@ -1,6 +0,0 @@
-module load cluster/genius/gpu_p100 PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
-python3 -m venv .venv --system-site-packages
-. .venv/bin/activate
-pip install pip --upgrade
-
-# pip install ... --no-cache-dir --no-build-isolation
